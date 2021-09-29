b1c6355dfbae581038ad451de153276fb2d625b2
Robin <robin@robinsuter.ch>
2021-09-29 10:38:58 -0400

    init project setup with eslint, prettier etc.

---
+++ eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: [
+    "react-app",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["jsx-a11y", "prettier"],
+  parser: "@typescript-eslint/parser",
+  rules: {
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+        ],
+        "newlines-between": "always",
+        alphabetize: { order: "asc", caseInsensitive: true },
+      },
+    ],
+  },
+};
